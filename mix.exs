bc2f613a77ef54bb6709a2be5c1e5a7907cc4c5b
Edison Cury <edisoncury@Edisons-MacBook-Pro.local>
2019-07-10 20:28:49 -0300

    Adds docs and published package.

--- mix.exs
+++ mix.exs
@@ -8,7 +8,10 @@ defmodule Casefy.MixProject do
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      name: "Casefy"
+      name: "Casefy",
+      description: description(),
+      package: package(),
+      source_url: "https://github.com/edicury/casefy"
     ]
   end
 
@@ -22,7 +25,23 @@ defmodule Casefy.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:recase, "~> 0.1"}
+      {:recase, "~> 0.1"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description() do
+    "Deeply nested objects casing made easy"
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "casefy",
+      # These are the default files included in the package
+      files: ~w(lib .formatter.exs mix.exs README* test),
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/edicury/casefy"}
     ]
   end
 end
